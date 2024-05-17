33b67c3a4b1e890320a26070f94c15518a85ec3e
joao <joaomarcosjmac@gmail.com>
2024-05-17 16:03:18 -0300

    ui clientes pronto

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -3,72 +3,189 @@ import {
   Button,
   Chip,
   Container,
+  IconButton,
+  InputBase,
   Paper,
   Stack,
   Typography,
-  alpha,
-  styled,
 } from "@mui/material"
 import NavBar from "../components/NavBar"
 import { DataGrid } from "@mui/x-data-grid"
-import { SearchOff } from "@mui/icons-material"
+import { Search } from "@mui/icons-material"
 
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
   {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
+    field: "nome",
+    headerName: "Nome Completo",
+    flex: 1,
   },
+  { field: "email", headerName: "Email", flex: 1 },
   {
-    field: "lastName",
-    headerName: "Last name",
-    width: 150,
-  },
-  {
-    field: "age",
-    headerName: "Age",
+    field: "idade",
+    headerName: "Idade",
     type: "number",
-    width: 110,
+    headerAlign: "left",
+    align: "left",
   },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-  },
+  { field: "celular", headerName: "Celular", flex: 1 },
+  // {
+  //   field: "access",
+  //   headerName: "Access Level",
+  //   flex: 1,
+  //   renderCell: ({ row: { access } }) => {
+  //     return (
+  //       <Box
+  //         width="60%"
+  //         m="0 auto"
+  //         p="5px"
+  //         display="flex"
+  //         justifyContent="center"
+  //         backgroundColor={
+  //           access === "admin"
+  //             ? colors.greenAccent[600]
+  //             : colors.greenAccent[700]
+  //         }
+  //         borderRadius="4px"
+  //       >
+  //         {access === "admin" && <AdminPanelSettingsOutlined />}
+  //         {access === "manager" && <SecurityOutlined />}
+  //         {access === "user" && <LockOpenOutlined />}
+  //         <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+  //           {access}
+  //         </Typography>
+  //       </Box>
+  //     )
+  //   },
+  // },
 ]
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  {
+    id: 1,
+    nome: "Jon Snow",
+    idade: 14,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 2,
+    nome: "Cersei Lannister",
+    idade: 31,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 3,
+    nome: "Jaime Lannister",
+    idade: 31,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 4,
+    nome: "Arya Stark",
+    idade: 11,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 5,
+    nome: "Daenerys Targaryen",
+    idade: null,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 6,
+    nome: "Melisandre",
+    idade: 150,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 7,
+    nome: "Ferrara Clifford",
+    idade: 44,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 8,
+    nome: "Rossini Frances",
+    idade: 36,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 9,
+    nome: "Harvey ",
+    idade: 65,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 10,
+    nome: "Jon Snow",
+    idade: 14,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 11,
+    nome: "Cersei Lannister",
+    idade: 31,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 12,
+    nome: "Jaime Lannister",
+    idade: 31,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 13,
+    nome: "Arya Stark",
+    idade: 11,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 14,
+    nome: "Daenerys Targaryen",
+    idade: null,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 15,
+    nome: "Melisandre",
+    idade: 150,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 16,
+    nome: "Ferrara Clifford",
+    idade: 44,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 17,
+    nome: "Rossini Frances",
+    idade: 36,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
+  {
+    id: 18,
+    nome: "Harvey ",
+    idade: 65,
+    email: "teste@gmail.com",
+    celular: "(21) 99999-9999",
+  },
 ]
 
 const Clientes = () => {
@@ -81,7 +198,7 @@ const Clientes = () => {
             <Box display={"flex"} justifyContent={"space-between"}>
               <Box>
                 <Typography variant="h5" fontWeight={"bold"}>
-                  Lista de Clientes <Chip label="5" variant="outlined" />
+                  Lista de Clientes <Chip label="18" variant="outlined" />
                 </Typography>
                 <Typography variant="h6">
                   Visualizar, adicionar, editar e excluir dados dos clientes.
@@ -91,24 +208,26 @@ const Clientes = () => {
                 <Button variant="contained">Adicionar</Button>
               </Box>
             </Box>
-            <Box display={"flex"} justifyContent={"space-between"}>
-              <Paper variant="outlined">
-                **CAMPO DE BUSCA E BUTTON PARA FILTRO**
+            <Box sx={{ display: "flex" }}>
+              <Paper variant="outlined" sx={{ display: "flex", flex: 1 }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Procurar pelo nome, email ou celular"
+                  inputProps={{ "aria-label": "search google maps" }}
+                />
+                <IconButton
+                  type="button"
+                  sx={{ p: "10px" }}
+                  aria-label="search"
+                >
+                  <Search />
+                </IconButton>
               </Paper>
             </Box>
-            <Box sx={{ height: 400, width: "100%" }}>
+            <Box sx={{ height: "70vh", width: "100%" }}>
               <DataGrid
                 rows={rows}
                 columns={columns}
-                initialState={{
-                  pagination: {
-                    paginationModel: {
-                      pageSize: 5,
-                    },
-                  },
-                }}
-                pageSizeOptions={[5]}
-                checkboxSelection
                 disableRowSelectionOnClick
               />
             </Box>
